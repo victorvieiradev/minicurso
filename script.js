b968f7c359fb1d821bb7ea978ec7f2bbb5be4613
Victor Vieira <contatovictorvieira@outlook.com>
2022-01-14 18:45:18 -0300

    implementar função para pegar json do rick and morty

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const expecie = document.querySelector('#expecie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((Response) => Response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        expecie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+
+    });
+}
+botao.onclick = pegarPersonagem;
