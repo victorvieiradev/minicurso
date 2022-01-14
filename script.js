f4b8473407c024658db2ca45aedfe4d4c43af219
Victor Vieira <contatovictorvieira@outlook.com>
2022-01-14 16:59:58 -0300

    capiturar elementos do html pelo js

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const expecie = document.querySelector('#expecie');
+const status = document.querySelector('#status');
